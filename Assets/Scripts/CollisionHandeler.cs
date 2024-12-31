b0477837ebffa289e02a06c7f7f340ce94bda6ad
Abhishek Sharma <abhisheksharmaprof1@gmail.com>
2025-01-01 01:21:24 +0530

    Final Vertical flow done and wrapped Up

--- Assets/Scripts/CollisionHandeler.cs
+++ Assets/Scripts/CollisionHandeler.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 public class CollisionHandeler : MonoBehaviour
 {
     [SerializeField] GameObject DestroyedShipVFX;
+    GameSceneManager gameSceneManager;
+
+    private void Start()
+    {
+        gameSceneManager = FindFirstObjectByType<GameSceneManager>();
+    }
     private void OnTriggerEnter(Collider other)
     {
+        gameSceneManager.ReloadLevel();
         Instantiate(DestroyedShipVFX, transform.position, transform.rotation);
         Destroy(gameObject);
     }
